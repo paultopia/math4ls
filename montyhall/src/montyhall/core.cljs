c5b42ecedad5d22af0592426bfa6d338de26d61e
Paul Gowder <paul.gowder@gmail.com>
2016-09-03 12:40:43 -0500

    hand-code bar chart

--- montyhall/src/montyhall/core.cljs
+++ montyhall/src/montyhall/core.cljs
@@ -11,13 +11,30 @@
 (defn more-data [] (reset! data (take 100 (repeatedly rand))))
 
 (defn hist [sample]
-  (-> (c/histogram sample :x-axis [0 1] :n-bins 10)
+  (-> (c/histogram sample :x-axis [0 1] :n-bins 30)
       (svg/as-svg :width 500 :height 200)))
 
+;; looks like b1 doesn't actually honor n-bins, making it useless.  raw svg?
+
+(defn svg-test []
+  [:svg {:x 0 :y 0 :width 50 :height 50}
+   [:rect {:x 10 :y 10 :width 1000 :height 10}]])
+
+(defn svg-bar-chart []
+  [:svg {:class "chart" :width 500 :height 500}
+   [:g {:transform "translate(0,0)"}
+    [:rect {:width 19 :height 80 :y 80}]]
+   [:g {:transform "translate(20,0)"}
+    [:rect {:width 19 :height 40 :y 120}]]])
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]
-   [hist @data]
-   [:button {:on-click more-data} "More data"]])
+  [:div [:h2 "graph me"]
+   ;; [hist @data]
+   ;; [:button {:on-click more-data} "More data"]
+   ;; [:div
+   ;;  [svg-test]]
+   [svg-bar-chart]
+   ])
 
 ;; -------------------------
 ;; Initialize app
