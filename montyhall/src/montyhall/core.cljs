d809be5bf5c5c762baa989e72e88f416866ba446
Paul Gowder <paul.gowder@gmail.com>
2016-09-03 13:43:40 -0500

    one strategy going

--- montyhall/src/montyhall/core.cljs
+++ montyhall/src/montyhall/core.cljs
@@ -6,10 +6,28 @@
 (def win (atom 0))
 (def loss (atom 0))
 
-(defn play-monty []
-  (do
-    (reset! win (rand-int 1000))
-    (reset! loss (rand-int 100))))
+(defn find-remaining
+  "Simulates opening (and thus eliminating) one of the goat doors, and
+  returns the remaining closed door, which player can switch to.
+
+  If player's first guess is goat, open other goat door, and return car as last
+  closed door.  If player's first guess is car, open one of the two goat doors and return other goat door as last closed door. "
+  [first-guess]
+  (if (= first-guess :car) :goat :car))
+
+(defn apply-strategy [strategy first-guess remaining]
+  (case strategy
+    :stubborn first-guess
+    :switcher remaining
+    :random (rand-nth [first-guess remaining])))
+
+(defn play-monty [strategy]
+  (let [first-guess (rand-nth [:goat :goat :car])
+        remaining (find-remaining first-guess)
+        result (apply-strategy strategy first-guess remaining)]
+    (cond
+      (= result :car) (swap! win inc)
+      :else (swap! loss inc))))
 
 (defn wl-chart [w l]
   (let [win-ratio (* 200 (/ w (+ w l)))
@@ -31,7 +49,7 @@
    [:p "test"]
    [:div
     [wl-chart @win @loss]]
-   [:button {:on-click play-monty} "shuffle"]])
+   [:button {:on-click #(play-monty :stubborn)} "shuffle"]])
 
 ;; -------------------------
 ;; Initialize app
