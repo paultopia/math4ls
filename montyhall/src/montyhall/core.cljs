7baf38c4d913db56d0705a9b6bdd21435b3adb47
Paul Gowder <paul.gowder@gmail.com>
2016-09-03 13:13:48 -0500

    incremental progress 2

--- montyhall/src/montyhall/core.cljs
+++ montyhall/src/montyhall/core.cljs
@@ -16,9 +16,21 @@
 
 ;; looks like b1 doesn't actually honor n-bins, making it useless.  raw svg?
 
-(defn svg-test []
-  [:svg {:x 0 :y 0 :width 50 :height 50}
-   [:rect {:x 10 :y 10 :width 1000 :height 10}]])
+(def win 509)
+(def loss 208)
+
+(defn wl-chart [w l]
+  (let [win-ratio (* 200 (/ w (+ w l)))
+        lose-ratio (- 200 win-ratio)]
+    [:div
+    [:svg {:class "chart" :width 150 :height 210}
+     [:g {:transform "translate(0,0)"}
+      [:rect {:width 30 :height win-ratio :y lose-ratio}]
+      [:text {:y 210 :x 20} "win"]]
+     [:g {:transform "translate(35,0)"}
+      [:rect {:width 30 :height lose-ratio :y win-ratio}]
+      [:text {:y 210 :x 25} "loss"]]]
+     [:p (str "w: " w " l: " l)]]))
 
 (defn svg-bar-chart []
   [:svg {:class "chart" :width 150 :height 210}
@@ -31,13 +43,11 @@
 
 (defn home-page []
   [:div [:h2 "graph me"]
-   ;; [hist @data]
-   ;; [:button {:on-click more-data} "More data"]
-   ;; [:div
-   ;;  [svg-test]]
    [svg-bar-chart]
    [svg-bar-chart]
-   ])
+   [:p "test"]
+   [:div
+    [wl-chart win loss]]])
 
 ;; -------------------------
 ;; Initialize app
