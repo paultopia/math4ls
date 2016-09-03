f553c1bbac4690be20d0391e2644eba1c423ede9
Paul Gowder <paul.gowder@gmail.com>
2016-09-03 13:18:15 -0500

    get rid of craptastic charting library

--- montyhall/project.clj
+++ montyhall/project.clj
@@ -6,8 +6,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.9.93" :scope "provided"]
-                 [reagent "0.6.0-rc"]
-                 [b1 "0.3.1"]]
+                 [reagent "0.6.0-rc"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]
             [lein-figwheel "0.5.4-5"]]
--- montyhall/src/montyhall/core.cljs
+++ montyhall/src/montyhall/core.cljs
@@ -1,20 +1,9 @@
 (ns montyhall.core
-  (:require [reagent.core :as reagent :refer [atom]]
-            [b1.charts :as c]
-            [b1.svg :as svg]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 ;; -------------------------
 ;; Views
 
-(def data (atom (take 100 (repeatedly rand))))
-
-(defn more-data [] (reset! data (take 100 (repeatedly rand))))
-
-(defn hist [sample]
-  (-> (c/histogram sample :x-axis [0 1] :n-bins 30)
-      (svg/as-svg :width 500 :height 200)))
-
-;; looks like b1 doesn't actually honor n-bins, making it useless.  raw svg?
 
 (def win 509)
 (def loss 208)
