380b1e8da4afe1e759aaed06d10b012e2cd1ba0f
Paul Gowder <paul.gowder@gmail.com>
2016-09-15 10:48:00 -0500

    stub

---
+++ bayesrule/README.md
@@ -0,0 +1,1 @@
+stub for future interactive bayes rule/base rate problem simulation. One big TODO.
---
+++ bayesrule/src/bayesrule/core.cljs
@@ -0,0 +1,17 @@
+(ns bayesrule.core
+    (:require [reagent.core :as reagent :refer [atom]]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Welcome to Reagent"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (reagent/render [home-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (mount-root))
